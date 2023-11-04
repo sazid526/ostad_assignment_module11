e6fe9b426dcd56c728095d5049b544ee28dff39b
Tanvir1943 <tanvir15-14261@diu.edu.bd>
2023-11-04 21:49:27 +0600

    done

---
+++ lib/screens/photos_details.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:ostad_assignment_module11/screens/photo_gallery_screen.dart';
+
+
+class PhotosDetails extends StatelessWidget {
+  const PhotosDetails({super.key, required this.photosInfo});
+
+  final PhotosInfo photosInfo;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Photos Details"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Image.network(photosInfo.url,width: double.infinity,height: 200,),
+            Text(photosInfo.title.toString()),
+            Center(child: Text(photosInfo.id.toString()))
+          ],
+        ),
+      ),
+    );
+  }
+}
